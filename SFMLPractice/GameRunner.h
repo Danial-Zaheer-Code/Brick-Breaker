300fb39512bfa4d704d439304a00db81f5582fad
danialzaheerfastian@outlook.com <danialzaheerfastian@outlook.com>
2025-07-27 21:04:35 +0500

    Created a GameRunner class to hold bussines logic

---
+++ SFMLPractice/GameRunner.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+using namespace sf;
+class GameRunner
+{
+private:
+	RectangleShape createRectangle(Vector2f size, Vector2f position, Color color)
+	{
+		RectangleShape rect(size);
+		rect.setFillColor(color);
+		rect.setPosition(position);
+		return rect;
+	}
+
+	CircleShape createBall()
+	{
+		CircleShape ball = CircleShape(7.f);
+		ball.setFillColor(Color::Red);
+		ball.setPosition(300.f, 300.f);
+		return ball;
+	}
+
+
+public:
+	GameRunner(): window(sf::VideoMode(1000, 600), "SFML Test")
+	{
+		ball = createBall();
+		player = createRectangle(Vector2f(65.f, 6.f), Vector2f(100.f, 100.f), Color::Green);
+	}
+	~GameRunner() = default;
+	void run()
+	{
+		while (window.isOpen()) {
+			Event event;
+
+			while (window.pollEvent(event)) {
+				if (event.type == Event::Closed)
+					window.close();
+			}
+			renderScreen();
+		}
+	}
+
+private:
+	void renderScreen()
+	{
+		window.clear(Color::Black);
+
+		/*for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 5; j++)
+			{
+				window.draw(level[i][j]);
+			}
+		}*/
+
+		window.draw(ball);
+		window.draw(player);
+		window.display();
+	}
+
+
+private:
+	RectangleShape level[5][5];
+	CircleShape ball;
+	RectangleShape player;
+	RenderWindow window;
+};
+
